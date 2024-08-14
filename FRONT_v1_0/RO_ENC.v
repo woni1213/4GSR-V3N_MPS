6da8b5a33fa6ea4d69d32f0ba1b6a2f2dcb26743
woni1213 <134993289+woni1213@users.noreply.github.com>
2024-08-14 11:12:34 +0900

    Fix - AXI4 Lite 주소 수정 외
    - RO_ENC 새로운 코드 적용
    - 그에 따라 Top, AXI 수정

--- FRONT_v1_0/RO_ENC.v
+++ FRONT_v1_0/RO_ENC.v
@@ -5,26 +5,32 @@
 MPS ROtary switch ENcorder Module
 개발 4팀 전경원 차장
 
-24.07.25 :	최초 생성
+24.08.05 :	최초 생성
 
 이성진 차장의 퇴사로 인하여 MPS PL 프로그래밍
 
 0. 기타
- - 동작 시방하게 되는데?
-	두칸씩 움직여야 한번 동작함
-	빠르게 돌리면 LCD 가끔 먹통됨
- - 원인 판별 필요 (PS or PL?)
- - 아마 PS가 원인일 확률이 매우 높음
- - 만약 원인이 PL이거나 PS에서 사용하기 어렵다면 RO_ENC_Test.v로 적용하여 테스트
+ - o_ro_enc_data가 아무리 빨리 돌려도 1이상 안올라감
 
 1. 개요
- - 
+ - RO_ENC.v 수정 코드
+ - 기존의 코드가 정상동작하면 폐기 요망
+ - """기존 코드가 거지같이 동작해서 해당 코드 사용"""
 
-2. 동작
- - a, b는 00 01 이런식으로 표기
- - 멈춰있을때는 00 or 11로 대기함
- - 한칸이 CW나 CCW로 동작 시 00 -> 01 or 10 -> 11로 변경됨
- - 11부터 시작시에도 마찬가지
+2. 수정 사항
+ - o_ro_enc_data 정보 변경 (이동 칸 수로 변경)
+ - Direction 추가
+
+3. 기능
+ - 여러칸 움직임을 감지 (Clear 후 다시 초기화)
+ - 비트로 움직임을 감지하지 않고 칸수 누적으로 변경 (o_ro_enc_data)
+ - 회전 방향 추가
+ - irq 추가 (알아서 판별 후 사용)
+
+4. 동작
+ - 동작이 감지되면 o_ro_enc_data에 칸수 누적 및 회전 방향 출력
+ - 동시에 irq 신호 출력 (0일때 동작 X)
+ - i_sw_intr_clear시 o_ro_enc_data를 0으로 초기화
 
 */
 
@@ -37,105 +43,58 @@ module RO_ENC
 	input i_ro_enc_state_b,
 
 	input i_sw_intr_clear,
-	output [1:0] o_ro_enc_data	
+	output o_ro_enc_irq,
+	output reg o_ro_enc_dir,			// 0 : CW, 1 : CCW
+	output reg [4:0] o_ro_enc_data
 );
 
-	parameter IDLE 	= 0;
-	parameter CW	= 1;	// o_ro_enc_data = 01, a가 먼저 변화하면
-	parameter CCW	= 2;	// o_ro_enc_data = 10, b가 먼저 변화하면
-	parameter LOW	= 3;	// o_ro_enc_data = 11, ab = 00
-	parameter HIGH	= 4;	// o_ro_enc_data = 00, ab = 11
+	reg [1:0] prev_ab;
+	reg [1:0] curr_ab;
+	wire [3:0] ab_state;
 
-	// FSM
-	reg [2:0] state;
-	reg [2:0] n_state;
-
-	// Flag
-	wire low_state;
-	wire high_state;
-
-	// FSM Control
 	always @(posedge i_clk or negedge i_rst)
     begin
-        if (~i_rst)
-            state <= IDLE;
+		if (~i_rst)
+		begin
+			prev_ab <= 0;
+			curr_ab <= 0;
+		end
 
-        else 
-			state <= n_state;
-    end
-
-	// FSM
-	always @(*)
-    begin
-        case (state)
-            IDLE :
-            begin
-                if (low_state)
-                    n_state <= LOW;
-
-				else if (high_state)
-					n_state <= HIGH;
-
-                else
-                    n_state <= IDLE;
-            end
-
-			LOW :
-            begin
-				if (i_sw_intr_clear)
-					n_state <= IDLE;
-
-                else if (i_ro_enc_state_a)
-                    n_state <= CW;
-
-				else if (i_ro_enc_state_b)
-					n_state <= CCW;
-
-                else
-                    n_state <= LOW;
-            end
-
-			HIGH :
-            begin
-				if (i_sw_intr_clear)
-					n_state <= IDLE;
-
-                else if (~i_ro_enc_state_a)
-                    n_state <= CW;
-
-				else if (~i_ro_enc_state_b)
-					n_state <= CCW;
-
-                else
-                    n_state <= HIGH;
-            end
-
-			CW :
-            begin
-				if (i_sw_intr_clear)
-					n_state <= IDLE;
-
-                else
-                    n_state <= CW;
-            end
-
-			CCW :
-            begin
-				if (i_sw_intr_clear)
-					n_state <= IDLE;
-
-                else
-                    n_state <= CCW;
-            end
-
-			default :
-                    n_state <= IDLE;
-		endcase
+		else
+		begin
+			prev_ab <= curr_ab;
+			curr_ab <= {i_ro_enc_state_a, i_ro_enc_state_b};
+		end
 	end
 
-	assign low_state = ~(i_ro_enc_state_a | i_ro_enc_state_b);
-	assign high_state = i_ro_enc_state_a & i_ro_enc_state_b;
+	always @(posedge i_clk or negedge i_rst)
+    begin
+		if (~i_rst || i_sw_intr_clear)
+		begin
+			o_ro_enc_dir <= 0;
+			o_ro_enc_data <= 0;
+		end
 
-	assign o_ro_enc_data = state[1:0];
+		else if ((ab_state == 4'b0010) || (ab_state == 4'b1101))
+		begin
+			o_ro_enc_dir <= 0;
+			o_ro_enc_data <= o_ro_enc_data + 1;
+		end
+
+		else if ((ab_state == 4'b0001) || (ab_state == 4'b1110))
+		begin
+			o_ro_enc_dir <= 1;
+			o_ro_enc_data <= o_ro_enc_data + 1;
+		end
+
+		else
+		begin
+			o_ro_enc_dir <= o_ro_enc_dir;
+			o_ro_enc_data <= o_ro_enc_data;
+		end
+	end
+
+	assign ab_state = {prev_ab, curr_ab};
+	assign o_ro_enc_irq = (o_ro_enc_data > 0) ? 1 : 0;
 
 endmodule
