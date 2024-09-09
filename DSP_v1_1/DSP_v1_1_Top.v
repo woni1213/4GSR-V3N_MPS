4338d2c776629f0bd6c62ff49875bbaabfcbb912
woni1213 <134993289+woni1213@users.noreply.github.com>
2024-09-09 16:38:54 +0900

    Update - SFP 추가 등
    - SFP 기능 추가
    - 변수 이름 수정
    - DPBRAM 수정

--- DSP_v1_1/DSP_v1_1_Top.v
+++ DSP_v1_1/DSP_v1_1_Top.v
@@ -2,23 +2,25 @@
 0. PS가 
     Interlock State. From INTL_v1_0인 i_INTL_state는 AXI로 PS가 읽어가게만 설정함
     o_Ready, o_Hart_beat 핀 만들기만 해놓음
+	SFP Handler 추가 해야함
 */
 module DSP_v1_1_Top #
 (
     parameter integer C_S_AXI_DATA_WIDTH = 32,
-	parameter integer C_S_AXI_ADDR_WIDTH = 8
+	parameter integer C_S_AXI_ADDR_NUM = 128,
+	parameter integer C_S_AXI_ADDR_WIDTH = $clog2(C_S_AXI_ADDR_NUM) + 2,
+
+	parameter integer C_AXIS_TDATA_WIDTH = 64,		// Frame Data Width
+	parameter integer C_NUMBER_OF_SLAVE = 3,		// Slave 수
+	parameter integer C_NUMBER_OF_FRAME = 6,		// Slave의 Frame 수
+
+	parameter integer C_DATA_STREAM_BIT = ((C_AXIS_TDATA_WIDTH) * (C_NUMBER_OF_SLAVE) * (C_NUMBER_OF_FRAME)),	// Stream Bit 수
+	parameter integer C_DATA_FRAME_BIT = ((C_AXIS_TDATA_WIDTH) * (C_NUMBER_OF_FRAME))					// Frame Bit 수
 )
 (
-    input i_Ethernet_connect,                       // 1 : Master / 0 : Slave
-    input [15:0] i_INTL_state,						// Interlock State. From INTL_v1_0
-
-    output o_Ready,
-	output o_Hart_beat,
-	output o_nMENPWM,
-	input i_valid,
-	input i_DSP_fail,
-
-    input i_DSP_nENPWM,
+	input [15:0] i_zynq_intl,						// Interlock Input
+	input i_pwm_en,									// PWM Enable (AND Gate IP)
+	output o_pwm_en,
 
     // From ADC
 	input [31:0] i_c_adc_data,						// Current ADC Floating Data
@@ -31,42 +33,27 @@ module DSP_v1_1_Top #
 	output [31:0] o_v_factor_axis_tdata,
 	output o_v_factor_axis_tvalid,
 
-    // DSP XINTF Data Line
-	output o_nZ_WE,
-	input i_nZ_B_WE,
-    input i_nZ_B_CS,
-    input [8:0] i_Z_B_XA,
-    inout [15:0] io_Z_B_XD,
+	// SFP Data
+	output [(C_NUMBER_OF_FRAME * C_AXIS_TDATA_WIDTH) * C_NUMBER_OF_SLAVE : 0] o_stream_data,
+	input [(C_NUMBER_OF_FRAME * C_AXIS_TDATA_WIDTH) * C_NUMBER_OF_SLAVE : 0] i_stream_data,
+
+	// SFP Flag
+	output o_aurora_tx_start_flag,				// SFP Tx 시작
+	input i_aurora_rx_end_flag,					// SFP Rx 종료
 
     // DPBRAM (XINTF) Bus Interface Ports Attribute
-	// Dual Clock
-	// M Port - Zynq / S Port - DSP
-	// Address length : 9 (Depth : 500) / Data Width : 16
+	// Address length : 9 (Depth : ) / Data Width : 16
+    (* X_INTERFACE_INFO = "HMT:JKW:s_dpbram_port:1.0 M_XINTF_W_DPBRAM addr0" *) output [8:0] o_xintf_w_ram_addr,
+    (* X_INTERFACE_INFO = "HMT:JKW:s_dpbram_port:1.0 M_XINTF_W_DPBRAM ce0" *) output o_xintf_w_ram_ce,
+    (* X_INTERFACE_INFO = "HMT:JKW:s_dpbram_port:1.0 M_XINTF_W_DPBRAM we0" *) output o_xintf_w_ram_we,
+    (* X_INTERFACE_INFO = "HMT:JKW:s_dpbram_port:1.0 M_XINTF_W_DPBRAM din0" *) output [15:0] o_xintf_w_ram_din,
+    (* X_INTERFACE_INFO = "HMT:JKW:s_dpbram_port:1.0 M_XINTF_W_DPBRAM dout0" *) input [15:0] i_xintf_w_ram_dout,
 
-    // Zynq System Clock
-    (* X_INTERFACE_INFO = "HMT:JKW:s_dpbram_port:1.0 M_XINTF_PL_W_DPBRAM addr0" *) output [8:0] o_xintf_PL_W_ram_addr,
-    (* X_INTERFACE_INFO = "HMT:JKW:s_dpbram_port:1.0 M_XINTF_PL_W_DPBRAM ce0" *) output o_xintf_PL_W_ram_ce,
-    (* X_INTERFACE_INFO = "HMT:JKW:s_dpbram_port:1.0 M_XINTF_PL_W_DPBRAM we0" *) output o_xintf_PL_W_ram_we,
-    (* X_INTERFACE_INFO = "HMT:JKW:s_dpbram_port:1.0 M_XINTF_PL_W_DPBRAM din0" *) output [15:0] o_xintf_PL_W_ram_din,
-    (* X_INTERFACE_INFO = "HMT:JKW:s_dpbram_port:1.0 M_XINTF_PL_W_DPBRAM dout0" *) input [15:0] i_xintf_PL_W_ram_dout,
-
-    (* X_INTERFACE_INFO = "HMT:JKW:m_dpbram_port:1.0 S_XINTF_DSP_R_DPBRAM addr1" *) output [8:0] o_xintf_DSP_R_ram_addr,
-    (* X_INTERFACE_INFO = "HMT:JKW:m_dpbram_port:1.0 S_XINTF_DSP_R_DPBRAM ce1" *) output o_xintf_DSP_R_ram_ce,
-    (* X_INTERFACE_INFO = "HMT:JKW:m_dpbram_port:1.0 S_XINTF_DSP_R_DPBRAM we1" *) output o_xintf_DSP_R_ram_we,
-    (* X_INTERFACE_INFO = "HMT:JKW:m_dpbram_port:1.0 S_XINTF_DSP_R_DPBRAM din1" *) output [15:0] o_xintf_DSP_R_ram_din,
-    (* X_INTERFACE_INFO = "HMT:JKW:m_dpbram_port:1.0 S_XINTF_DSP_R_DPBRAM dout1" *) input [15:0] i_xintf_DSP_R_ram_dout,
-
-    (* X_INTERFACE_INFO = "HMT:JKW:s_dpbram_port:1.0 M_XINTF_PL_R_DPBRAM addr0" *) output [8:0] o_xintf_PL_R_ram_addr,
-    (* X_INTERFACE_INFO = "HMT:JKW:s_dpbram_port:1.0 M_XINTF_PL_R_DPBRAM ce0" *) output o_xintf_PL_R_ram_ce,
-    (* X_INTERFACE_INFO = "HMT:JKW:s_dpbram_port:1.0 M_XINTF_PL_R_DPBRAM we0" *) output o_xintf_PL_R_ram_we,
-    (* X_INTERFACE_INFO = "HMT:JKW:s_dpbram_port:1.0 M_XINTF_PL_R_DPBRAM din0" *) output [15:0] o_xintf_PL_R_ram_din,
-    (* X_INTERFACE_INFO = "HMT:JKW:s_dpbram_port:1.0 M_XINTF_PL_R_DPBRAM dout0" *) input [15:0] i_xintf_PL_R_ram_dout,
-    
-    (* X_INTERFACE_INFO = "HMT:JKW:m_dpbram_port:1.0 S_XINTF_DSP_W_DPBRAM addr1" *) output [8:0] o_xintf_DSP_W_ram_addr,
-    (* X_INTERFACE_INFO = "HMT:JKW:m_dpbram_port:1.0 S_XINTF_DSP_W_DPBRAM ce1" *) output o_xintf_DSP_W_ram_ce,
-    (* X_INTERFACE_INFO = "HMT:JKW:m_dpbram_port:1.0 S_XINTF_DSP_W_DPBRAM we1" *) output o_xintf_DSP_W_ram_we,
-    (* X_INTERFACE_INFO = "HMT:JKW:m_dpbram_port:1.0 S_XINTF_DSP_W_DPBRAM din1" *) output [15:0] o_xintf_DSP_W_ram_din,
-    (* X_INTERFACE_INFO = "HMT:JKW:m_dpbram_port:1.0 S_XINTF_DSP_W_DPBRAM dout1" *) input [15:0] i_xintf_DSP_W_ram_dout,
+    (* X_INTERFACE_INFO = "HMT:JKW:s_dpbram_port:1.0 M_XINTF_R_DPBRAM addr0" *) output [8:0] o_xintf_r_ram_addr,
+    (* X_INTERFACE_INFO = "HMT:JKW:s_dpbram_port:1.0 M_XINTF_R_DPBRAM ce0" *) output o_xintf_r_ram_ce,
+    (* X_INTERFACE_INFO = "HMT:JKW:s_dpbram_port:1.0 M_XINTF_R_DPBRAM we0" *) output o_xintf_r_ram_we,
+    (* X_INTERFACE_INFO = "HMT:JKW:s_dpbram_port:1.0 M_XINTF_R_DPBRAM din0" *) output [15:0] o_xintf_r_ram_din,
+    (* X_INTERFACE_INFO = "HMT:JKW:s_dpbram_port:1.0 M_XINTF_R_DPBRAM dout0" *) input [15:0] i_xintf_r_ram_dout,
 
     // AXI4 Lite Bus Interface Ports
 	input wire  s00_axi_aclk,
@@ -97,130 +84,101 @@ module DSP_v1_1_Top #
     output [8:0] o_W_addr_pointer,
     output [8:0] o_R_addr_pointer
 );
-    wire [15:0] w_DSP_System_Status;
-    wire [31:0] w_DSP_Set_C;
-    wire [31:0] w_DSP_Set_V;
-    wire [31:0] w_DSP_Current_P_Gain;
-    wire [31:0] w_DSP_Current_I_Gain;
-    wire [31:0] w_DSP_Current_D_Gain;
-    wire [31:0] w_DSP_Voltage_P_Gain;
-    wire [31:0] w_DSP_Voltage_I_Gain;
-    wire [31:0] w_DSP_Voltage_D_Gain;
-    wire [31:0] w_DSP_Max_Duty;
-    wire [31:0] w_DSP_Max_Phase;
-    wire [31:0] w_DSP_Max_Freq;
-    wire [31:0] w_DSP_Min_Freq;
-    wire [15:0] w_DSP_Deadband_Set;
-    wire [15:0] w_DSP_Switching_Freq_Set;
-    wire [31:0] w_DSP_PI_Max_V;
-    wire [31:0] w_DSP_PI_Min_V;
-    wire [31:0] w_DSP_PI_Max_C;
-    wire [31:0] w_DSP_PI_Min_C;
-    wire [31:0] w_DSP_Current_Duty;
-    wire [31:0] w_DSP_Current_Phase;
-    wire [31:0] w_DSP_Current_Freq;
-    wire [15:0] w_DSP_Intr;
-    wire [15:0] w_DSP_Firmware_Ver;
+	wire sfp_m_en;
+	wire pwm_en;
+	wire zynq_intl;
 
-    wire [31:0] r_c_adc_data;
-    wire [31:0] r_v_adc_data;
-    wire [15:0] r_DSP_System_Status;
-    wire [31:0] r_DSP_Set_C;
-    wire [31:0] r_DSP_Set_V;
-    wire [31:0] r_DSP_Current_P_Gain;
-    wire [31:0] r_DSP_Current_I_Gain;
-    wire [31:0] r_DSP_Current_D_Gain;
-    wire [31:0] r_DSP_Voltage_P_Gain;
-    wire [31:0] r_DSP_Voltage_I_Gain;
-    wire [31:0] r_DSP_Voltage_D_Gain;
-    wire [31:0] r_DSP_Max_Duty;
-    wire [31:0] r_DSP_Max_Phase;
-    wire [31:0] r_DSP_Max_Freq;
-    wire [31:0] r_DSP_Min_Freq;
-    wire [15:0] r_DSP_Deadband_Set;
-    wire [15:0] r_DSP_Switching_Freq_Set;
-    wire [31:0] r_DSP_PI_Max_V;
-    wire [31:0] r_DSP_PI_Min_V;
-    wire [31:0] r_DSP_PI_Max_C;
-    wire [31:0] r_DSP_PI_Min_C;
-    wire [31:0] r_DSP_Current_Duty;
-    wire [31:0] r_DSP_Current_Phase;
-    wire [31:0] r_DSP_Current_Freq;
-    wire [15:0] r_DSP_Intr;
-    wire [15:0] r_DSP_Firmware_Ver;
-    wire [15:0] r_PI_Intr_Status;
-    wire [31:0] r_WF_Counter;
+    wire [15:0] zynq_status;
+	wire [15:0] o_zynq_ver;
+    wire [31:0] set_c;
+    wire [31:0] set_v;
+    wire [31:0] p_gain_c;
+    wire [31:0] i_gain_c;
+    wire [31:0] d_gain_c;
+    wire [31:0] p_gain_v;
+    wire [31:0] i_gain_v;
+    wire [31:0] d_gain_v;
+    wire [31:0] max_duty;
+    wire [31:0] max_phase;
+    wire [31:0] max_freq;
+    wire [31:0] min_freq;
+    wire [15:0] deadband;
+    wire [15:0] sw_freq;
+    wire [31:0] max_c;
+    wire [31:0] min_c;
+	wire [31:0] max_v;
+    wire [31:0] min_v;
+    wire [31:0] master_pi_param;
+
+    wire [15:0] dsp_status;
+	wire [15:0] i_dsp_ver;
+	wire [31:0] slave_pi_param_1;
+	wire [31:0] slave_pi_param_2;
+	wire [31:0] slave_pi_param_3;
+	wire [31:0] wf_read_cnt;
+
+	wire [(C_NUMBER_OF_FRAME * C_AXIS_TDATA_WIDTH) * C_NUMBER_OF_SLAVE : 0] rx_axi_data;
+	wire [(C_NUMBER_OF_FRAME * C_AXIS_TDATA_WIDTH) * C_NUMBER_OF_SLAVE : 0] tx_axi_data;
 
 	AXI4_Lite_S01 #
 	(
 		.C_S_AXI_DATA_WIDTH(C_S_AXI_DATA_WIDTH),
-		.C_S_AXI_ADDR_WIDTH(C_S_AXI_ADDR_WIDTH)
+		.C_S_AXI_ADDR_NUM(C_S_AXI_ADDR_NUM),
+		.C_S_AXI_ADDR_WIDTH(C_S_AXI_ADDR_WIDTH),
+
+		.C_DATA_STREAM_BIT(C_DATA_STREAM_BIT),
+		.C_DATA_FRAME_BIT(C_DATA_FRAME_BIT)
 	)
 	u_AXI4_Lite_S01
 	(
-		// Write (PS - PL)
-        .o_DSP_System_Status(w_DSP_System_Status),
-        .o_DSP_Set_C(w_DSP_Set_C),
-        .o_DSP_Set_V(w_DSP_Set_V),
-        .o_DSP_Current_P_Gain(w_DSP_Current_P_Gain),
-        .o_DSP_Current_I_Gain(w_DSP_Current_I_Gain),
-        .o_DSP_Current_D_Gain(w_DSP_Current_D_Gain),
-        .o_DSP_Voltage_P_Gain(w_DSP_Voltage_P_Gain),
-        .o_DSP_Voltage_I_Gain(w_DSP_Voltage_I_Gain),
-        .o_DSP_Voltage_D_Gain(w_DSP_Voltage_D_Gain),
-        .o_DSP_Max_Duty(w_DSP_Max_Duty),
-        .o_DSP_Max_Phase(w_DSP_Max_Phase),
-        .o_DSP_Max_Freq(w_DSP_Max_Freq),
-        .o_DSP_Min_Freq(w_DSP_Min_Freq),
-        .o_DSP_Deadband_Set(w_DSP_Deadband_Set),
-        .o_DSP_Switching_Freq_Set(w_DSP_Switching_Freq_Set),
-        .o_DSP_PI_Max_V(w_DSP_PI_Max_V),
-        .o_DSP_PI_Min_V(w_DSP_PI_Min_V),
-        .o_DSP_PI_Max_C(w_DSP_PI_Max_C),
-        .o_DSP_PI_Min_C(w_DSP_PI_Min_C),
-        .o_DSP_Current_Duty(w_DSP_Current_Duty),
-        .o_DSP_Current_Phase(w_DSP_Current_Phase),
-        .o_DSP_Current_Freq(w_DSP_Current_Freq),
-        .o_DSP_Intr(w_DSP_Intr),
-        .o_DSP_Firmware_Ver(w_DSP_Firmware_Ver),
-
-        .o_c_factor(o_c_factor_axis_tdata),
+		// ADC Calc Factor
+		.o_c_factor(o_c_factor_axis_tdata),
 		.o_v_factor(o_v_factor_axis_tdata),
 
-		// Read (PL - PS)
-        .i_valid(i_valid),
-        .i_DSP_fail(i_DSP_fail),
-        .i_DSP_nENPWM(i_DSP_nENPWM),
+		// ADC Data
+		.i_c_adc_data(i_c_adc_data),
+        .i_v_adc_data(i_v_adc_data),
 
-        .i_c_adc_data(r_c_adc_data),
-        .i_v_adc_data(r_v_adc_data),
-        .i_DSP_System_Status(r_DSP_System_Status),
-        .i_DSP_Set_C(r_DSP_Set_C),
-        .i_DSP_Set_V(r_DSP_Set_V),
-        .i_DSP_Current_P_Gain(r_DSP_Current_P_Gain),
-        .i_DSP_Current_I_Gain(r_DSP_Current_I_Gain),
-        .i_DSP_Current_D_Gain(r_DSP_Current_D_Gain),
-        .i_DSP_Voltage_P_Gain(r_DSP_Voltage_P_Gain),
-        .i_DSP_Voltage_I_Gain(r_DSP_Voltage_I_Gain),
-        .i_DSP_Voltage_D_Gain(r_DSP_Voltage_D_Gain),
-        .i_DSP_Max_Duty(r_DSP_Max_Duty),
-        .i_DSP_Max_Phase(r_DSP_Max_Phase),
-        .i_DSP_Max_Freq(r_DSP_Max_Freq),
-        .i_DSP_Min_Freq(r_DSP_Min_Freq),
-        .i_DSP_Deadband_Set(r_DSP_Deadband_Set),
-        .i_DSP_Switching_Freq_Set(r_DSP_Switching_Freq_Set),
-        .i_DSP_PI_Max_V(r_DSP_PI_Max_V),
-        .i_DSP_PI_Min_V(r_DSP_PI_Min_V),
-        .i_DSP_PI_Max_C(r_DSP_PI_Max_C),
-        .i_DSP_PI_Min_C(r_DSP_PI_Min_C),
-        .i_DSP_Current_Duty(r_DSP_Current_Duty),
-        .i_DSP_Current_Phase(r_DSP_Current_Phase),
-        .i_DSP_Current_Freq(r_DSP_Current_Freq),
-        .i_DSP_Intr(r_DSP_Intr),
-        .i_DSP_Firmware_Ver(r_DSP_Firmware_Ver),
-        .i_PI_Intr_Status(r_PI_Intr_Status),
-        .i_WF_Counter(r_WF_Counter),
-        .i_INTL_state(i_INTL_state),
+		// System Control
+		.o_sfp_m_en(sfp_m_en),
+		.i_pwm_en(i_pwm_en),
+		.o_pwn_en(o_pwm_en),
+		.i_zynq_intl(i_zynq_intl),
+
+		// DPBRAM Write
+        .o_zynq_status(zynq_status),
+		.o_zynq_firmware_ver(o_zynq_ver),
+        .o_set_c(set_c),
+        .o_set_v(set_v),
+        .o_p_gain_c(p_gain_c),
+        .o_i_gain_c(i_gain_c),
+        .o_d_gain_c(d_gain_c),
+        .o_p_gain_v(p_gain_v),
+        .o_i_gain_v(i_gain_v),
+        .o_d_gain_v(d_gain_v),
+        .o_max_duty(max_duty),
+        .o_max_phase(max_phase),
+        .o_max_freq(max_freq),
+        .o_min_freq(min_freq),
+		.o_max_c(max_c),
+        .o_min_c(min_c),
+        .o_max_v(max_v),
+        .o_min_v(min_v),
+		.o_deadband(deadband),
+        .o_sw_freq(sw_freq),
+
+		// DPBRAM Read
+        .i_dsp_status(dsp_status),
+		.i_dsp_firmware_ver(i_dsp_ver),
+		.i_wf_read_cnt(wf_read_cnt),
+
+		// SFP Data
+		.i_slave_pi_param_1(slave_pi_param_1),			// Slave PI Parameter Receive to DSP (SFP Master Mode)
+		.i_slave_pi_param_2(slave_pi_param_2),
+		.i_slave_pi_param_3(slave_pi_param_3),
+		.o_master_stream_data(tx_axi_data),				// SFP Master Mode Data to Slave
+		.i_msater_stream_data(rx_axi_data),				// SFP Master Mode Data from Slave
+		.o_master_pi_param(master_pi_param),
 
 		.S_AXI_ACLK(s00_axi_aclk),
 		.S_AXI_ARESETN(s00_axi_aresetn),
@@ -251,71 +209,49 @@ module DSP_v1_1_Top #
         .i_clk(s00_axi_aclk),
         .i_rst(s00_axi_aresetn),
 
-        .i_Ethernet_connect(i_Ethernet_connect),
+        .i_sfp_m_en(sfp_m_en),
+		.i_i_zynq_intl(zynq_intl),
 
-        // WRITE
-        .o_xintf_PL_W_ram_addr(o_xintf_PL_W_ram_addr),
-        .o_xintf_PL_W_ram_din(o_xintf_PL_W_ram_din),
+        // DPBRAM WRITE
+        .o_xintf_w_ram_addr(o_xintf_w_ram_addr),
+        .o_xintf_w_ram_din(o_xintf_w_ram_din),
+		.o_xintf_w_ram_ce(o_xintf_w_ram_ce),
 
         .i_c_adc_data(i_c_adc_data),
         .i_v_adc_data(i_v_adc_data),
-        .i_DSP_System_Status(w_DSP_System_Status),
-        .i_DSP_Set_C(w_DSP_Set_C),
-        .i_DSP_Set_V(w_DSP_Set_V),
-        .i_DSP_Current_P_Gain(w_DSP_Current_P_Gain),
-        .i_DSP_Current_I_Gain(w_DSP_Current_I_Gain),
-        .i_DSP_Current_D_Gain(w_DSP_Current_D_Gain),
-        .i_DSP_Voltage_P_Gain(w_DSP_Voltage_P_Gain),
-        .i_DSP_Voltage_I_Gain(w_DSP_Voltage_I_Gain),
-        .i_DSP_Voltage_D_Gain(w_DSP_Voltage_D_Gain),
-        .i_DSP_Max_Duty(w_DSP_Max_Duty),
-        .i_DSP_Max_Phase(w_DSP_Max_Phase),
-        .i_DSP_Max_Freq(w_DSP_Max_Freq),
-        .i_DSP_Min_Freq(w_DSP_Min_Freq),
-        .i_DSP_Deadband_Set(w_DSP_Deadband_Set),
-        .i_DSP_Switching_Freq_Set(w_DSP_Switching_Freq_Set),
-        .i_DSP_PI_Max_V(w_DSP_PI_Max_V),
-        .i_DSP_PI_Min_V(w_DSP_PI_Min_V),
-        .i_DSP_PI_Max_C(w_DSP_PI_Max_C),
-        .i_DSP_PI_Min_C(w_DSP_PI_Min_C),
-        .i_DSP_Current_Duty(w_DSP_Current_Duty),
-        .i_DSP_Current_Phase(w_DSP_Current_Phase),
-        .i_DSP_Current_Freq(w_DSP_Current_Freq),
-        .i_DSP_Intr(w_DSP_Intr),
-        .i_DSP_Firmware_Ver(w_DSP_Firmware_Ver),
-        
-        // READ
-        .i_xintf_PL_R_ram_dout(i_xintf_PL_R_ram_dout),
-        .o_xintf_PL_R_ram_addr(o_xintf_PL_R_ram_addr),
+        .i_zynq_status(zynq_status),
+		.i_zynq_firmware_ver(o_zynq_ver),
+		.i_set_c(set_c),
+        .i_set_v(set_v),
+        .i_p_gain_c(p_gain_c),
+        .i_i_gain_c(i_gain_c),
+        .i_d_gain_c(d_gain_c),
+        .i_p_gain_v(p_gain_v),
+        .i_i_gain_v(i_gain_v),
+        .i_d_gain_v(d_gain_v),
+        .i_max_duty(max_duty),
+        .i_max_phase(max_phase),
+        .i_max_freq(max_freq),
+        .i_min_freq(min_freq),
+        .i_max_v(max_v),
+        .i_min_v(min_v),
+        .i_max_c(max_c),
+        .i_min_c(min_c),
+		.i_master_pi_param(master_pi_param),			// Master PI Parameter Send to DSP (SFP Slave Mode)
+		.i_deadband(deadband),
+        .i_sw_freq(sw_freq),
 
-        .o_c_adc_data(r_c_adc_data),
-        .o_v_adc_data(r_v_adc_data),
-        .o_DSP_System_Status(r_DSP_System_Status),
-        .o_DSP_Set_C(r_DSP_Set_C),
-        .o_DSP_Set_V(r_DSP_Set_V),
-        .o_DSP_Current_P_Gain(r_DSP_Current_P_Gain),
-        .o_DSP_Current_I_Gain(r_DSP_Current_I_Gain),
-        .o_DSP_Current_D_Gain(r_DSP_Current_D_Gain),
-        .o_DSP_Voltage_P_Gain(r_DSP_Voltage_P_Gain),
-        .o_DSP_Voltage_I_Gain(r_DSP_Voltage_I_Gain),
-        .o_DSP_Voltage_D_Gain(r_DSP_Voltage_D_Gain),
-        .o_DSP_Max_Duty(r_DSP_Max_Duty),
-        .o_DSP_Max_Phase(r_DSP_Max_Phase),
-        .o_DSP_Max_Freq(r_DSP_Max_Freq),
-        .o_DSP_Min_Freq(r_DSP_Min_Freq),
-        .o_DSP_Deadband_Set(r_DSP_Deadband_Set),
-        .o_DSP_Switching_Freq_Set(r_DSP_Switching_Freq_Set),
-        .o_DSP_PI_Max_V(r_DSP_PI_Max_V),
-        .o_DSP_PI_Min_V(r_DSP_PI_Min_V),
-        .o_DSP_PI_Max_C(r_DSP_PI_Max_C),
-        .o_DSP_PI_Min_C(r_DSP_PI_Min_C),
-        .o_DSP_Current_Duty(r_DSP_Current_Duty),
-        .o_DSP_Current_Phase(r_DSP_Current_Phase),
-        .o_DSP_Current_Freq(r_DSP_Current_Freq),
-        .o_DSP_Intr(r_DSP_Intr),
-        .o_DSP_Firmware_Ver(r_DSP_Firmware_Ver),
-        .o_PI_Intr_Status(r_PI_Intr_Status),
-        .o_WF_Counter(r_WF_Counter),
+        // DPBRAM READ
+        .i_xintf_r_ram_dout(i_xintf_r_ram_dout),
+        .o_xintf_r_ram_addr(o_xintf_r_ram_addr),
+		.o_xintf_r_ram_ce(o_xintf_r_ram_ce),
+
+        .o_dsp_status(dsp_status),
+		.o_dsp_firmware_ver(i_dsp_ver),
+		.o_wf_read_cnt(wf_read_cnt),
+		.o_slave_pi_param_1(slave_pi_param_1),			// Slave PI Parameter Receive to DSP (SFP Master Mode)
+		.o_slave_pi_param_2(slave_pi_param_2),
+		.o_slave_pi_param_3(slave_pi_param_3),
 
         // Debugging
         .o_debug_r_state(o_debug_r_state),
@@ -324,30 +260,35 @@ module DSP_v1_1_Top #
         .o_W_addr_pointer(o_W_addr_pointer)
     );
 
-    assign o_nMENPWM = i_DSP_nENPWM;
+	SFP_Handler #
+	(
+		.C_AXIS_TDATA_WIDTH(C_AXIS_TDATA_WIDTH),
+		.C_DATA_STREAM_BIT(C_DATA_STREAM_BIT),
+		.C_DATA_FRAME_BIT(C_DATA_FRAME_BIT)
+	)
+	u_SFP_Handler
+	(
+		.i_clk(i_clk),
+		.i_rst(i_rst),
+
+		.i_axi_data(tx_axi_data),
+		.o_axi_data(rx_axi_data),
+
+		.i_stream_data(i_stream_data),
+		.o_stream_data(o_stream_data),
+
+		.i_sfp_end_flag(i_aurora_rx_end_flag),
+		.o_sfp_start_flag(o_aurora_tx_start_flag),
+
+		.i_sfp_m_en(sfp_m_en)
+	);
 
     assign o_c_factor_axis_tvalid = 1;
     assign o_v_factor_axis_tvalid = 1;
 
-    assign o_xintf_PL_W_ram_ce = 1;
-    assign o_xintf_PL_W_ram_we = 1;
+    assign o_xintf_w_ram_we = 1;
+    assign o_xintf_r_ram_we = 0;
 
-    assign o_xintf_PL_R_ram_ce = 1;
-    assign o_xintf_PL_R_ram_we = 0;
-
-    assign o_xintf_PL_R_ram_din = 0;
-
-    assign o_nZ_WE = o_xintf_DSP_R_ram_we;
-
-    assign o_xintf_DSP_R_ram_addr = i_Z_B_XA;
-    assign o_xintf_DSP_R_ram_din = 0;
-    assign o_xintf_DSP_R_ram_ce = (i_nZ_B_WE) ? ~i_nZ_B_CS : 0;
-    assign o_xintf_DSP_R_ram_we = ~i_nZ_B_WE;
-    assign io_Z_B_XD = (o_xintf_DSP_R_ram_ce && ~o_xintf_DSP_R_ram_we) ? i_xintf_DSP_R_ram_dout : 16'hZZZZ;
-
-    assign o_xintf_DSP_W_ram_addr = i_Z_B_XA;
-    assign o_xintf_DSP_W_ram_ce = (~i_nZ_B_WE) ? ~i_nZ_B_CS : 0;
-    assign o_xintf_DSP_W_ram_we = ~i_nZ_B_WE;
-    assign o_xintf_DSP_W_ram_din = io_Z_B_XD;  
+	assign zynq_intl = (i_zynq_intl != 0);
 
 endmodule
