cd925b0575502e6e608b4685a69adac0d3d2dfc0
Gnade77 <143045823+Gnade77@users.noreply.github.com>
2024-09-24 17:51:26 +0900

    Create - WF
    Waveform 관련 코드 신규 추가

---
+++ WF_v1_0/AXI4_Lite_S04.v
@@ -0,0 +1,256 @@
+`timescale 1 ns / 1 ps
+
+module AXI4_Lite_S04 #
+(
+	parameter integer C_S_AXI_DATA_WIDTH	= 0,
+	parameter integer C_S_AXI_ADDR_NUM 		= 0,
+	parameter integer C_S_AXI_ADDR_WIDTH	= 0
+)
+(
+	// DPBRAM Write
+	output reg o_wf_write_en,
+	output reg [9:0] o_wf_write_addr,
+	output reg [15:0] o_wf_write_data,
+
+	// DPBRAM Read
+	input [31:0] i_wf_read_data_num,
+
+	input S_AXI_ACLK,
+	input S_AXI_ARESETN,
+	input [C_S_AXI_ADDR_WIDTH - 1 : 0] S_AXI_AWADDR,
+	input [2:0] S_AXI_AWPROT,
+	input S_AXI_AWVALID,
+	output S_AXI_AWREADY,
+	input [C_S_AXI_DATA_WIDTH - 1 : 0] S_AXI_WDATA,
+	input [(C_S_AXI_DATA_WIDTH / 8) - 1 : 0] S_AXI_WSTRB,
+	input S_AXI_WVALID,
+	output S_AXI_WREADY,
+	output [1:0] S_AXI_BRESP,
+	output wire S_AXI_BVALID,
+	input S_AXI_BREADY,
+	input [C_S_AXI_ADDR_WIDTH - 1 : 0] S_AXI_ARADDR,
+	input [2 : 0] S_AXI_ARPROT,
+	input S_AXI_ARVALID,
+	output S_AXI_ARREADY,
+	output [C_S_AXI_DATA_WIDTH - 1 : 0] S_AXI_RDATA,
+	output [1 : 0] S_AXI_RRESP,
+	output S_AXI_RVALID,
+	input S_AXI_RREADY
+);
+
+	reg [C_S_AXI_ADDR_WIDTH - 1 : 0] axi_awaddr;
+	reg axi_awready;
+	reg axi_wready;
+	reg [1:0] axi_bresp;
+	reg axi_bvalid;
+	reg [C_S_AXI_ADDR_WIDTH-1 : 0] axi_araddr;
+	reg axi_arready;
+	reg [C_S_AXI_DATA_WIDTH-1 : 0] axi_rdata;
+	reg [1:0] axi_rresp;
+	reg axi_rvalid;
+
+	localparam integer ADDR_LSB = 2;
+	localparam integer OPT_MEM_ADDR_BITS = $clog2(C_S_AXI_ADDR_NUM) - 1;
+
+	// slv_reg IO Type Select. 0 : Input, 1 : Output
+	// slv_reg Start to LSB
+	localparam [C_S_AXI_ADDR_NUM - 1 : 0] io_sel = 1111;	// 0 : Input, 1 : Output
+
+	reg [C_S_AXI_DATA_WIDTH - 1 : 0] slv_reg[C_S_AXI_ADDR_NUM - 1 : 0];
+
+	wire slv_reg_rden;
+	wire slv_reg_wren;
+	reg [C_S_AXI_DATA_WIDTH-1:0] reg_data_out;
+	integer byte_index;
+	reg aw_en;
+
+	genvar i;
+	integer j;
+
+	// Address Write Flag
+	always @( posedge S_AXI_ACLK )
+	begin
+		if ( S_AXI_ARESETN == 1'b0 )
+		begin
+			axi_awready <= 1'b0;
+			aw_en <= 1'b1;
+		end
+
+		else
+		begin
+			if (~axi_awready && S_AXI_AWVALID && S_AXI_WVALID && aw_en)
+			begin
+				axi_awready <= 1'b1;
+				aw_en <= 1'b0;
+			end
+
+			else if (S_AXI_BREADY && axi_bvalid)
+			begin
+				aw_en <= 1'b1;
+				axi_awready <= 1'b0;
+			end
+
+			else
+	          axi_awready <= 1'b0;
+	    end 
+	end
+
+	// Address Write
+	always @( posedge S_AXI_ACLK )
+	begin
+		if ( S_AXI_ARESETN == 1'b0 )
+			axi_awaddr <= 0;
+
+		else
+		begin
+			if (~axi_awready && S_AXI_AWVALID && S_AXI_WVALID && aw_en)
+				axi_awaddr <= S_AXI_AWADDR;
+		end
+
+	end
+
+	// Write Data Flag
+	always @( posedge S_AXI_ACLK )
+	begin
+		if ( S_AXI_ARESETN == 1'b0 )
+			axi_wready <= 1'b0;
+
+		else
+		begin
+			if (~axi_wready && S_AXI_WVALID && S_AXI_AWVALID && aw_en )
+				axi_wready <= 1'b1;
+			else
+				axi_wready <= 1'b0;
+		end 
+	end
+
+	// Write Data (M to S)
+	generate
+	for (i = 0; i < C_S_AXI_ADDR_NUM; i = i + 1)
+	begin
+		always @( posedge S_AXI_ACLK )
+		begin
+		if (io_sel[i])
+		begin
+			if (S_AXI_ARESETN == 1'b0)
+				slv_reg[i] <= 0;
+
+			else if (slv_reg_wren)
+				if (axi_awaddr[ADDR_LSB+OPT_MEM_ADDR_BITS:ADDR_LSB] == i)
+					for ( byte_index = 0; byte_index <= (C_S_AXI_DATA_WIDTH/8)-1; byte_index = byte_index+1 )
+						if ( S_AXI_WSTRB[byte_index] == 1 ) 
+							slv_reg[i][(byte_index*8) +: 8] <= S_AXI_WDATA[(byte_index*8) +: 8];
+
+			else
+				slv_reg[i] <= slv_reg[i];
+			end
+		end
+	end
+	endgenerate
+
+	// Response Flag
+	always @( posedge S_AXI_ACLK )
+	begin
+		if ( S_AXI_ARESETN == 1'b0 )
+		begin
+			axi_bvalid  <= 0;
+			axi_bresp   <= 2'b0;
+		end
+
+		else
+		begin
+			if (axi_awready && S_AXI_AWVALID && ~axi_bvalid && axi_wready && S_AXI_WVALID)
+			begin
+				axi_bvalid <= 1'b1;
+				axi_bresp  <= 2'b0;
+			end
+
+			else
+			begin
+				if (S_AXI_BREADY && axi_bvalid) 
+					axi_bvalid <= 1'b0; 
+			end
+		end
+	end
+
+	// Address Read Flag
+	always @( posedge S_AXI_ACLK )
+	begin
+		if ( S_AXI_ARESETN == 1'b0 )
+		begin
+			axi_arready <= 1'b0;
+			axi_araddr  <= 32'b0;
+		end
+
+		else
+		begin
+			if (~axi_arready && S_AXI_ARVALID)
+			begin
+				axi_arready <= 1'b1;
+				axi_araddr  <= S_AXI_ARADDR;
+			end
+
+			else
+				axi_arready <= 1'b0;
+		end
+	end
+
+	// Read Data Flag
+	always @( posedge S_AXI_ACLK )
+	begin
+		if ( S_AXI_ARESETN == 1'b0 )
+		begin
+			axi_rvalid <= 0;
+			axi_rresp  <= 0;
+		end 
+
+		else
+		begin
+			if (axi_arready && S_AXI_ARVALID && ~axi_rvalid)
+			begin
+				axi_rvalid <= 1'b1;
+				axi_rresp  <= 2'b0;
+			end
+
+			else if (axi_rvalid && S_AXI_RREADY)
+				axi_rvalid <= 1'b0;
+		end
+	end
+
+	// Read Data (S to M)
+	always @(*)
+	begin
+		reg_data_out = 0;
+
+		for (j = 0; j < C_S_AXI_ADDR_NUM; j = j + 1)
+			if (axi_araddr[ADDR_LSB + OPT_MEM_ADDR_BITS : ADDR_LSB] == j)
+				reg_data_out = slv_reg[j];
+	end
+
+	always @( posedge S_AXI_ACLK )
+	begin
+		if ( S_AXI_ARESETN == 1'b0 )
+			axi_rdata  <= 0;
+
+		else
+		begin
+			if (slv_reg_rden)
+				axi_rdata <= reg_data_out;
+		end
+	end
+
+	// Input
+	always @(posedge S_AXI_ACLK)
+	begin
+		slv_reg[4]			<= i_wf_read_data_num;
+	end
+
+	// Output
+	always @(posedge S_AXI_ACLK)
+	begin
+		o_wf_write_en		<= slv_reg[0];			// PS에서 Waveform data를 줄 때 1로 설정해야 함
+		o_wf_write_addr		<= slv_reg[1];
+		o_wf_write_data		<= slv_reg[2];
+	end
+
+endmodule
---
+++ WF_v1_0/WF.v
@@ -0,0 +1,174 @@
+module WF
+(
+	input i_clk,
+	input i_rst,
+
+	input i_dsp_wf_start,
+	output reg o_dsp_wf_mode,
+
+	input [31:0] i_wf_read_cnt,
+
+	input i_wf_write_en,
+
+	// DPBRAM WRITE
+	output reg [8:0] o_xintf_wf_ram_addr,
+	output reg [9:0] o_xintf_wf_ram_din,
+	output reg o_xintf_wf_ram_ce,
+
+	input [9:0] i_wf_write_addr,
+	input [15:0] i_wf_write_data,
+
+	output reg [31:0] o_wf_read_data_num
+);
+	parameter W_IDLE = 0;
+	parameter W_SETUP = 1;
+	parameter WRITE = 2;
+	parameter W_DONE = 3;
+
+	parameter DSP_IDLE = 0;
+	parameter DSP_RUN = 1;
+	parameter DSP_DONE = 2;
+
+	// FSM
+	reg [1:0] w_state;
+	reg [1:0] n_w_state;
+
+	reg [1:0] dsp_state;
+	reg [1:0] n_dsp_state;
+
+	always @(posedge i_clk or negedge i_rst)
+	begin
+		if (~i_rst)
+			w_state <= W_IDLE;
+
+		else
+			w_state <= n_w_state;
+	end
+
+	// DPBRAM CE Control
+	always @(posedge i_clk or negedge i_rst)
+    begin
+            if (~i_rst)
+				o_xintf_wf_ram_ce <= 0;
+
+			else if ((w_state == W_SETUP) || (w_state == WRITE))
+				o_xintf_wf_ram_ce <= 1;
+
+			else
+				o_xintf_wf_ram_ce <= 0;
+	end
+
+	// FSM
+	always @(*)
+	begin
+		case (w_state)
+			W_IDLE :
+			begin
+				if (i_wf_write_en)
+					n_w_state <= W_SETUP;
+
+				else
+					n_w_state <= W_IDLE;
+			end
+
+			W_SETUP :
+				n_w_state <= WRITE;
+
+			WRITE :
+				n_w_state <= W_DONE;
+
+			W_DONE :
+			begin
+				if (!i_wf_write_en)
+					n_w_state <= W_IDLE;
+
+				else
+					n_w_state <= W_DONE;
+			end
+			
+			default: 
+				n_w_state <= W_IDLE;
+		endcase
+	end
+
+	// DPBRAM Write
+	always @(posedge i_clk or negedge i_rst)
+	begin
+		if (~i_rst)
+		begin
+			o_xintf_wf_ram_addr <= 0;
+            o_xintf_wf_ram_din <= 0;
+		end
+
+		else if (w_state == WRITE)
+		begin
+			o_xintf_wf_ram_addr <= i_wf_write_addr;
+			o_xintf_wf_ram_din <= i_wf_write_data;
+		end
+				
+		else
+			o_xintf_wf_ram_addr <= 0;
+	end
+
+	always @(posedge i_clk or negedge i_rst)
+	begin
+		if (~i_rst)
+			dsp_state <= DSP_IDLE;
+
+		else
+			dsp_state <= n_dsp_state;
+	end
+
+	always @(posedge i_clk or negedge i_rst)
+	begin
+		if (~i_rst)
+			o_wf_read_data_num <= 0;
+
+		else if (dsp_state == DSP_RUN)
+			o_wf_read_data_num <= o_wf_read_data_num + 1;
+
+		else
+			o_wf_read_data_num <= 0;
+	end
+
+	// FSM
+	always @(*)
+	begin
+		case (dsp_state)
+			DSP_IDLE :
+			begin
+				if (i_dsp_wf_start)
+				begin
+					n_dsp_state <= DSP_RUN;
+					o_dsp_wf_mode <= 1;
+				end	
+
+				else
+					n_dsp_state <= DSP_IDLE;
+			end
+
+			DSP_RUN :
+			begin
+				if (o_wf_read_data_num == (i_wf_read_cnt - 1))
+				begin
+					n_dsp_state <= DSP_DONE;
+					o_dsp_wf_mode <= 0;
+				end
+			end
+
+			DSP_DONE :
+			begin
+				if (!i_dsp_wf_start)
+					n_dsp_state <= DSP_IDLE;
+
+				else
+					n_dsp_state <= DSP_DONE;
+			end
+			
+			default: 
+				n_dsp_state <= DSP_IDLE;
+		endcase
+	end
+
+
+endmodule
